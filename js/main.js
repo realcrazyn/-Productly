006381795ac9d16d7987d46531d0e1ad3f3ca38e
alex <realcrazyn@yandex.ru>
2021-01-13 19:27:33 +0300

    Ready
    готово с адаптивом

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-$(function(){
+$(function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1175,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: true,
+                }
+            },
+
+
+        ]
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
+
+    });
 
 });
